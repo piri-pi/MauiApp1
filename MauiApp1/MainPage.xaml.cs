50e1d24b277c776136de7d08f2c21482669f2f62
Bernhard Tarmann <bernhard@blasiusberg.at>
2022-08-14 13:35:31 +0200

    Projektdateien hinzufügen.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	public ObservableCollection<Item> Data { get; set; } = new();
+	public MainPage()
+	{
+		InitializeComponent();
+		BindingContext = this;
+	}
+
+	private void Add_Item(object sender, EventArgs e)
+	{
+		Data.Add(new Item() { Name = "BlaBla"});
+	}
+
+	private void Add_SubItem(object sender, EventArgs e)
+	{
+		var item = (sender as Button).BindingContext as Item;
+		item.SubItems.Add(new SubItem() { Comment = "BlubBlub" });
+	}
+}
+
+public partial class Item : ObservableObject
+{
+	[ObservableProperty]
+	string name;
+
+	[ObservableProperty]
+	ObservableCollection<SubItem> subItems = new();
+}
+
+public partial class SubItem : ObservableObject
+{
+	[ObservableProperty]
+	string comment;
+}
